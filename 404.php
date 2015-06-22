7c7558e39a5a9070337db5db2e91a01dd4a4d62f
jimiofeden <adam.hollock@gmail.com>
2015-06-22 14:40:18 -0400

    Testing 404 change.

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	<h1>404 Not Found</h1>
+	<h1>404 Not Found!</h1>
 	<p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
 	<?php get_search_form(); ?>
 
